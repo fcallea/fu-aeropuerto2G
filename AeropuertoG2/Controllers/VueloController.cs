ffbe4e337aad7d43e7bd033b6c7be7c76238169c
fcallea <52554932+fcallea@users.noreply.github.com>
2022-10-22 00:00:03 -0400

    Front Vuelos Asignacion

--- AeropuertoG2/Controllers/VueloController.cs
+++ AeropuertoG2/Controllers/VueloController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,11 +14,10 @@ namespace AeropuertoG2.Controllers
 {
     public class VueloController : Controller
     {
+        [HttpGet]
         public ActionResult AsignarVuelo()
         {
-
-
-
+            //CARGANDO LISTA DE TRIPULANTES
             var url = "https://localhost:44365/api/Tripulacion/ListarTripulacion";
             //var url = "https://tripulacion20220916123257.azurewebsites.net/api/tripulacion";
             var request = (HttpWebRequest)WebRequest.Create(url);
@@ -26,18 +26,16 @@ namespace AeropuertoG2.Controllers
             request.Method = "GET";
             request.Timeout = 300000;
             var json = ""; 
-
             // Invocación del servicio
             var response = request.GetResponse();
             var stream = response.GetResponseStream();
             var sr = new StreamReader(stream);
             var Resultado = sr.ReadToEnd();
-
             var m = JsonConvert.DeserializeObject<List<AsignacionTripulacionViewModel>>(Resultado);
             Session["listaTripulacionAsig"] = m;
 
 
-
+            //CARGANDO LISTA DE AERONAVES
             url = "https://localhost:44365/api/Aeronave/ListarAeronaves";
             request = (HttpWebRequest)WebRequest.Create(url);
             request.Accept = "application/json";
@@ -45,18 +43,16 @@ namespace AeropuertoG2.Controllers
             request.Method = "GET";
             request.Timeout = 300000;
             json = "";
-
             // Invocación del servicio
             response = request.GetResponse();
             stream = response.GetResponseStream();
             sr = new StreamReader(stream);
             Resultado = sr.ReadToEnd();
-
             var n = JsonConvert.DeserializeObject<List<AsignarVueloAeronaveModel>>(Resultado);
             Session["listarAeronaveAsig"] = n;
 
 
-
+            //CARGANDO LISTA DE DESTINOS VUELOS
             url = "https://localhost:44365/api/Vuelo/ListarDestinosVuelo";
             request = (HttpWebRequest)WebRequest.Create(url);
             request.Accept = "application/json";
@@ -64,18 +60,75 @@ namespace AeropuertoG2.Controllers
             request.Method = "GET";
             request.Timeout = 300000;
             json = "";
-
             // Invocación del servicio
             response = request.GetResponse();
             stream = response.GetResponseStream();
             sr = new StreamReader(stream);
             Resultado = sr.ReadToEnd();
-
             var o = JsonConvert.DeserializeObject<List<AsignarDestinosVueloModel>>(Resultado);
             Session["listarDestinosVueloAsig"] = o;
 
+
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult AsignarVuelo(Models.principales datos, string submitButton)
+        {
+
+            AsingarVueloModel modeloVuelo = new AsingarVueloModel();
+            modeloVuelo.idVuelo = datos.AsignarDestinosVueloModel.Id;
+            modeloVuelo.listaItinerarios.Add(new ItinerarioVueloModel
+            {
+                idTripulacion = datos.AsignacionTripulacionViewModel.idTripulacion,
+                idAeronave = datos.AsignarVueloAeronaveModel.Id,
+                fechaHoraPartida = DateTime.Now,
+                fechaHoraAbordaje = DateTime.Now,
+                zonaAbordaje = "A",
+                nroPuertaAbordaje = "B1",
+                fechaHoraLLegada = DateTime.Now,
+                tipoVuelo = "COMERCIAL"
+            });
+
+
+
+            switch (submitButton)
+            {
+                case "AsignarValue":
+                        var url = "https://localhost:44365/api/Vuelo/AsignarVuelo";
+                        //var url = "https://tripulacion20220916123257.azurewebsites.net/api/tripulacion";
+                        var request = (HttpWebRequest)WebRequest.Create(url);
+                        request.Accept = "application/json";
+                        request.ContentType = "application/json";
+                        request.Method = "POST";
+                        request.Timeout = 300000;
+                        var json = JsonConvert.SerializeObject(modeloVuelo);
+                        //request.ContentLength.b = json.Length;
+
+                        ASCIIEncoding encoding = new ASCIIEncoding();
+                        Byte[] bytes = encoding.GetBytes(json);
+
+                        Stream newStream = request.GetRequestStream();
+                        newStream.Write(bytes, 0, bytes.Length);
+                        newStream.Close();
+
+                        // Invocación del servicio
+                        var response = request.GetResponse();
+                        var stream = response.GetResponseStream();
+                        var sr = new StreamReader(stream);
+                        var Resultado = sr.ReadToEnd();
+
+                        var m = Resultado;
+                        Session["idVueloAsignado"] = m;
+                        //dataGridView1.DataSource = Resultado;
+                        //return Task.CompletedTask;
+                    break;
+            }
+            return View();
+        }
+
+
         /*
         // GET: Vuelo
         public ActionResult Index()
--- AeropuertoG2/Models/AsignarVuelo.cs
+++ AeropuertoG2/Models/AsignarVuelo.cs
@@ -12,6 +12,7 @@ namespace AeropuertoG2.Models
         public AsignacionTripulacionViewModel AsignacionTripulacionViewModel { get; set; }
         public AsignarVueloAeronaveModel AsignarVueloAeronaveModel { get; set; }
         public AsignarDestinosVueloModel AsignarDestinosVueloModel { get; set; }
+        public AsingarVueloModel AsingarVueloModel { get; set; }
     }
 
     public class AsignacionTripulacionViewModel
@@ -44,5 +45,28 @@ namespace AeropuertoG2.Models
         public Guid IdAeropuertoDestino { get; set; }
         public string NombreAeropuertoDestino { get; set; }
         public string DestinoVueloNombre { get; set; }
+    }
+
+    public class AsingarVueloModel
+    {
+        public Guid idVuelo { get; set; }
+        public List<ItinerarioVueloModel> listaItinerarios { get; set; }
+
+        public AsingarVueloModel()
+        {
+            listaItinerarios = new List<ItinerarioVueloModel>();
+        }
+    }
+
+    public class ItinerarioVueloModel
+    {
+        public Guid idTripulacion { get; set; }
+        public Guid idAeronave { get; set; }
+        public DateTime fechaHoraPartida { get; set; }
+        public DateTime fechaHoraAbordaje { get; set; }
+        public string zonaAbordaje { get; set; }
+        public string nroPuertaAbordaje { get; set; }
+        public DateTime fechaHoraLLegada { get; set; }
+        public string tipoVuelo { get; set; }
     }
 }
